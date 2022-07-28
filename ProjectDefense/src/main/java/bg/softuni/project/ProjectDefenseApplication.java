c15cbcb33b41c3f47697fb59611af2a68e84c5c5
Teodor Semerdzhiev <tsemerdzhiev@vmobile.eu>
2022-07-28 15:22:00 +0300

    empty project created

---
+++ ProjectDefense/src/main/java/bg/softuni/project/ProjectDefenseApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDefenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectDefenseApplication.class, args);
+    }
+
+}
---
+++ ProjectDefense/src/test/java/bg/softuni/project/ProjectDefenseApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDefenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
